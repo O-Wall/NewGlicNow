a8f3a39cc722f13a076df8ec9d981d2dae90e325
VICTOR SOARES DE SOUZA OSORIO <victor.ssosorio@senacsp.edu.br>
2024-06-20 19:43:30 -0300

    Classe Global

---
+++ NewGlicNow/Global.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NewGlicNow
+{
+    public static class Global
+    {
+        public static string Conexao = string.Empty;
+        public static string Servidor = string.Empty;
+        public static string Banco = string.Empty;
+        public static int IdUsuarioLogado = 0;
+
+        public static string Criptografar(string senha)
+        {
+            Byte[] original;
+            Byte[] criptografado;
+            MD5 md5 = new MD5CryptoServiceProvider();
+            original = ASCIIEncoding.Default.GetBytes(senha);
+            criptografado = md5.ComputeHash(original);
+
+            return Regex.Replace(BitConverter.ToString(criptografado), "-", "").ToLower();
+        }
+        public static void LerAppConfig()
+        {
+            Servidor = ConfigurationManager.AppSettings.Get("servidor");
+            Banco = ConfigurationManager.AppSettings.Get("banco");
+
+            Conexao = $"Data Source={Servidor};Initial Catalog={Banco};Integrated Security=true;";
+        }
+    }
+}
