6e20af315cea1944b5c180db26db2ae8ad3ff004
Victor SS <142058707+Victor-SS@users.noreply.github.com>
2024-06-25 03:21:50 -0300

    aaaaaaaaaaaaaaaaaaaaa

---
+++ NewGlicNow/MapaGlic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewGlicNow
+{
+    public  class MapaGlic
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public int PreCafe {  get; set; }
+        public int PosCafe { get; set;}
+        public int PreAlmoco { get; set; }
+        public int PosAlmoco { get; set; }
+        public int PreJantar { get; set; }
+        public int PosJantar { get; set; }
+        public int BasalMatutino { get; set; }
+        public int BasalNoturno { get; set; }
+        public string Observacao { get; set; }
+        public int UsuarioId { get; set; }
+
+        public MapaGlic()
+        {
+            Id = 0;
+            Data = DateTime.MinValue;
+            PreCafe = 0;
+            PosCafe = 0;
+            PreAlmoco = 0;
+            PosAlmoco = 0;
+            PreJantar = 0;
+            PosJantar = 0;
+            BasalMatutino = 0;
+            BasalNoturno = 0;
+            Observacao = string.Empty;
+            UsuarioId = 0;
+        }
+    }
+}
--- NewGlicNow/ucCadastroMapGlic.Designer.cs
+++ NewGlicNow/ucCadastroMapGlic.Designer.cs
@@ -31,14 +31,14 @@
             this.txtValores = new System.Windows.Forms.TextBox();
             this.txtObservacao = new System.Windows.Forms.TextBox();
             this.cboPeriodo = new System.Windows.Forms.ComboBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lblValores = new System.Windows.Forms.Label();
             this.lblPeriodo = new System.Windows.Forms.Label();
             this.lblObservacao = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.dgvMapaGlic = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMapaGlic)).BeginInit();
             this.SuspendLayout();
             // 
             // txtValores
@@ -65,15 +65,6 @@
             this.cboPeriodo.Name = "cboPeriodo";
             this.cboPeriodo.Size = new System.Drawing.Size(152, 21);
             this.cboPeriodo.TabIndex = 11;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox1.Location = new System.Drawing.Point(25, 26);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(639, 265);
-            this.pictureBox1.TabIndex = 10;
-            this.pictureBox1.TabStop = false;
             // 
             // lblValores
             // 
@@ -115,27 +106,43 @@
             this.panel1.Size = new System.Drawing.Size(639, 157);
             this.panel1.TabIndex = 14;
             // 
-            // button1
+            // btnGravar
             // 
-            this.button1.Location = new System.Drawing.Point(547, 473);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 35);
-            this.button1.TabIndex = 15;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnGravar.Location = new System.Drawing.Point(547, 473);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(103, 35);
+            this.btnGravar.TabIndex = 15;
+            this.btnGravar.Text = "&Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            // 
+            // dgvMapaGlic
+            // 
+            this.dgvMapaGlic.AllowUserToAddRows = false;
+            this.dgvMapaGlic.AllowUserToDeleteRows = false;
+            this.dgvMapaGlic.AllowUserToResizeColumns = false;
+            this.dgvMapaGlic.AllowUserToResizeRows = false;
+            this.dgvMapaGlic.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvMapaGlic.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMapaGlic.Location = new System.Drawing.Point(25, 25);
+            this.dgvMapaGlic.MultiSelect = false;
+            this.dgvMapaGlic.Name = "dgvMapaGlic";
+            this.dgvMapaGlic.ReadOnly = true;
+            this.dgvMapaGlic.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMapaGlic.Size = new System.Drawing.Size(639, 266);
+            this.dgvMapaGlic.TabIndex = 16;
             // 
             // ucCadastroMapGlic
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dgvMapaGlic);
+            this.Controls.Add(this.btnGravar);
             this.Controls.Add(this.panel1);
             this.Name = "ucCadastroMapGlic";
             this.Size = new System.Drawing.Size(684, 524);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMapaGlic)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -145,11 +152,11 @@
         private System.Windows.Forms.TextBox txtValores;
         private System.Windows.Forms.TextBox txtObservacao;
         private System.Windows.Forms.ComboBox cboPeriodo;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblValores;
         private System.Windows.Forms.Label lblPeriodo;
         private System.Windows.Forms.Label lblObservacao;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.DataGridView dgvMapaGlic;
     }
 }
